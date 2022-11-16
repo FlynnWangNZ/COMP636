ce62cc85d2e7deff92e29f8bf632f69d17846b90
FlynnWangNZ <fei.wang@lincolnuni.ac.nz>
2022-11-16 19:44:03 +1300

    answer to quiz of session 1

---
+++ Session 1/quiz/variables.py
@@ -0,0 +1,21 @@
+# Question 1
+# abd
+
+# Question 2
+# a
+
+# Question3
+age = 435
+age = age * 2
+name = "Santa"
+name = name * 3
+
+# Question 4
+age = 435
+print(age)
+print(age + 1)
+print('Happy Birthday!')
+
+# Question 5
+santa_hometown = "North Pole"
+sleigh_speed = 1000.19283

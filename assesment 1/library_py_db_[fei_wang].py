ee0e9dc046a7ff786674986a05f8cb4a0c8a8680
FlynnWangNZ <fei.wang@lincolnuni.ac.nz>
2022-12-01 13:26:44 +1300

    assessment 1

---
+++ assesment 1/library_py_db_[fei_wang].py
@@ -0,0 +1,250 @@
+####### WAIKIRIKIRI LIBRARY MANAGEMENT SYSTEM #######
+# Name:  Fei Wang
+# Student ID: 1153888
+####################################################
+
+import mysql.connector
+from mysql.connector import FieldType
+import datetime
+import connect_library
+import re
+
+dbconn = None
+
+def getCursor():
+    global dbconn
+    global connection
+    connection = mysql.connector.connect(
+        user=connect_library.dbuser, 
+        password=connect_library.dbpass, 
+        host=connect_library.dbhost,
+        database=connect_library.dbname, 
+        autocommit=True
+    )
+    dbconn = connection.cursor()
+    return dbconn
+
+
+def columnOutput(dbData,cols,formatStr):
+# dbData is a list of tuples
+# cols is a dictionary with column name as the key and data type as the item
+# formatStr uses the following format, with one set of curly braces {} for each column.
+# For each column "{: <10}" determines the width of the column, padded with spaces (10 spaces in this example)
+#   <, ^ and > determine the alignment of the text: < (left aligned), ^ (centre aligned), > (right aligned)
+#   The following example is for 3 columns of output: left-aligned, 5 characters wide; centred, 10 characters; right-aligned 15 characters:
+#       formatStr = "{: <5}  {: ^10}  {: >15}"
+# You can also pad with something other than a space and put characters between the columns, 
+# e.g. this pads with full stops '.' and separates the columns with the pipe character | :
+#       formatStr = "{:.<5} | {:.^10} | {:.>15}"
+    print(formatStr.format(*cols))
+    for row in dbData:
+        rowList=list(row)
+        for index,item in enumerate(rowList):
+            if item==None:      # Removes any None values from the rowList, which would cause the print(*rowList) to fail
+                rowList[index]=""       # Replaces them with an empty string
+            elif type(item)==datetime.date or type(item)==datetime.datetime or type(item)==datetime.time or type(item)==datetime.timedelta:    # If item is a date, date-time, time or timedelta, convert to a string to avoid formatting issues
+                rowList[index]=str(item)
+        print(formatStr.format(*rowList))   
+
+
+def listBooks():
+    cur = getCursor()
+    # book list with number of copy orderd by year of publication desc and then category asc and then title asc
+    # number of copy will be 0 if there is no copy of a book
+    cur.execute("select b.bookid, b.booktitle, b.category, b.yearofpublication, count(bc.bookid) as copynumber from books b left join bookcopies bc on b.bookid = bc.bookid group by b.bookid order by b.yearofpublication desc, b.category, b.booktitle;")
+    dbOutput = cur.fetchall()
+    colOutputDict = {desc[0]:FieldType.get_info(desc[1]) for desc in cur.description}  # creates a dictionary with column name as the key and data type as the item
+    print("\nBOOKS\n")
+    formatStr = "{: <7}  {: <50}  {: <20}  {: <20}  {: <10}"    # See columnOutput function for details on formatting options
+    columnOutput(dbOutput,colOutputDict,formatStr)
+
+
+def listBorrowers():
+    cur = getCursor()
+    # show all borrowers with the order of familiname asc and then firstname asc
+    cur.execute("select * from borrowers order by familyname, firstname;")
+    dbOutPut = cur.fetchall()
+    colOutputDict = {desc[0]:FieldType.get_info(desc[1]) for desc in cur.description}
+    print("\nBORROWERS\n")
+    formatStr = "{: <15}  {: <10}  {: <10}  {: <15}  {: <20}  {: <20}  {: <15}  {: <15}  {: <15}"
+    columnOutput(dbOutPut, colOutputDict, formatStr)
+
+
+def listBookcopies(bookid=None):
+    cur = getCursor()
+    # show all book copies of specific bookid
+    if bookid:
+        querySql = f"select * from bookcopies where bookid = {bookid}"
+    else:
+        querySql = "select * from bookcopies order by bookid, bookcopyid"
+    cur.execute(querySql)
+    dbOutput = cur.fetchall()
+    colOutputDict = {desc[0]:FieldType.get_info(desc[1]) for desc in cur.description}
+    print("\nBORROWERS\n")
+    formatStr = "{: <15}  {: <10}  {: <20}"
+    columnOutput(dbOutput, colOutputDict, formatStr)
+
+
+def getNewValue(dbCur):
+    columnNames = [desc[0] for desc in dbCur.description]
+    displayText = ""
+    for index in range(1, len(columnNames)):
+        displayText += f"{index} - {columnNames[index]}\n"
+    displayText += "0 <-Back\nPlease select the column INDEX to be updated: "
+    columnIndex = input(displayText)
+    try:
+        columnIndex = int(columnIndex)
+    except Exception as e:
+        print("You are supposed to enter an integer.")
+    
+    if columnIndex == 0:
+        return
+
+    if 0 < columnIndex < len(columnNames):
+        columnName = columnNames[columnIndex]
+        columnType = FieldType.get_info(dbCur.description[columnIndex][1])
+        newValue = input("Please enter the value: ")
+        if columnType == 'DATE':
+            # A simplified expression to check date format
+            if re.search('^[0-9]{4}-[0-9]{2}-[0-9]{2}$', newValue):
+                newValue = f"str_to_date('{newValue}', '%Y-%m-%d')"
+            else:
+                raise ValueError("Date value should be in format of 'yyyy-mm-dd'")
+        else:
+            newValue = f"'{newValue}'"
+
+        return columnName, newValue
+    else:
+        raise ValueError("The index you entered is not valid.")
+
+
+def updateBorrower():
+    # list all borrowers
+    listBorrowers()
+    # get user intpu borrower id
+    borrower_id = input("Please enter the borrower id you want update: ")
+    try:  # borrower id should be a number
+        borrower_id = int(borrower_id)
+    except Exception as e:
+        print("\n Error. You should enter a number rather than anything else.")
+    # fetch data from db of the specific borrower id
+    cur = getCursor()
+    cur.execute(f"select * from borrowers where borrowerid = {borrower_id}")
+    dbOutPut = cur.fetchall()
+    if dbOutPut:  # borrower id is valid
+        try:
+            columnName, newValue = getNewValue(cur)  # call a function to get valid column name and value
+            updateSql = f"update borrowers set {columnName} = {newValue} where borrowerid = {borrower_id}"
+            cur.execute(updateSql)  # execute the update SQL
+            print("Value has been updated.")  # print result to user
+        except ValueError as e:
+            print(e)
+    else:  # borrower id does not exist
+        print("There is no such borrower.")
+    
+    
+def addLoan():
+    # input borrower id and check if it is valid
+    listBorrowers()
+    cur = getCursor()
+    borrowerid = input("Please input the borrower id: ")
+    try:
+        borrowerid = int(borrowerid)
+    except Exception as e:
+        print("Borrower id should be a number.")
+        return
+    cur.execute(f"select * from borrowers where borrowerid = {borrowerid}")
+    dbOutput = cur.fetchall()
+    if not dbOutput:
+        print(f"The borrower id {borrowerid} does not exist.")
+        return
+    
+    # input book copy id and check if it is valid
+    listBookcopies()
+    cur = getCursor()
+    bookcopyid = input("Please input the book copy id: ")
+    try:
+        bookcopyid = int(bookcopyid)
+    except Exception as e:
+        print("Book copy id should be a number.")
+        return
+    cur.execute(f"select * from bookcopies where bookcopyid = {bookcopyid}")
+    dbOutput = cur.fetchall()
+    if not dbOutput:
+        print(f"The book copy id {bookcopyid} does not exist.")
+        return
+
+    # insert a record of book loan
+    cur = getCursor()
+    cur.execute(f"insert into loans values(null, {bookcopyid}, {borrowerid}, curdate(), 0)")
+    print("Add loan finished.")
+
+
+def reportOverdueBooks():
+    # get overdue books
+    sql = "select b.booktitle, br.firstname, br.familyname, curdate()-l.loandate as numberofdays from books b inner join bookcopies bc on b.bookid=bc.bookid inner join loans l on bc.bookcopyid=l.bookcopyid inner join borrowers br on br.borrowerid = l.borrowerid where curdate()-l.loandate>35 and l.returned=0"
+    cur = getCursor()
+    cur.execute(sql)
+    dbOutput = cur.fetchall()
+    colOutputDict = {desc[0]:FieldType.get_info(desc[1]) for desc in cur.description}
+    print("\nOVERDUE\n")
+    formatStr = "{: <50}  {: <10}  {: <10}  {: <15}"
+    columnOutput(dbOutput,colOutputDict,formatStr)
+
+
+def reportMostLoanedBooks():
+    # list all loaned books order by times desc
+    sql = "select b.bookid, b.booktitle, count(*) as borrowedtimes from books b, bookcopies bc, loans l where b.bookid = bc.bookid and bc.bookcopyid = l.bookcopyid group by b.bookid order by borrowedtimes desc"
+    cur = getCursor()
+    cur.execute(sql)
+    dbOutput = cur.fetchall()
+    colOutputDict = {desc[0]:FieldType.get_info(desc[1]) for desc in cur.description}
+    print("\nOVERDUE\n")
+    formatStr = "{: <10}  {: <50}  {: <10}"
+    columnOutput(dbOutput,colOutputDict,formatStr)
+
+
+#function to display the menu
+def dispMenu():
+    print("==== WELCOME TO WAIKIRIKIRI LIBRARY MANAGEMENT SYSTEM ===")
+    print("1 - Book List")
+    print("2 - Borrower List")
+    print("3 - Update Borrower")
+    print("4 - Overdue Books")
+    print("5 - Most Loaned Books")
+    print("6 - Loan Book")
+    print("Q - Quit")
+    response = input("Please select menu choice: ")
+    return response.upper()  # Convert all input content into UPPER case
+
+# ******** MAIN PROGRAM *********
+
+#Display the menu and prompt the user to select an item
+response = dispMenu()
+
+#repeat this loop until user enters a "Q"
+while response != "Q":
+    if response == "1":
+        listBooks()
+    elif response == "2":
+        listBorrowers()
+    elif response == "3":
+        updateBorrower()
+    elif response == "4":
+        reportOverdueBooks()
+    elif response == "5":
+        reportMostLoanedBooks()
+    elif response == "6":
+        addLoan()
+    elif response == "R":  # Continue the loop
+        continue
+    else:
+        print("Invalid response, please re-enter.")
+
+    input("\nPress Enter to continue.")
+    #Display the menu and prompt the user to select an item
+    response = dispMenu()
+
+print("=== Thank you for using WAIKIRIKIRI LIBRARY MANAGEMENT SYSTEM ===")
+
+
